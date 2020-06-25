a06eb6bec0a5af7c05b2d0b45eaf8c4a725582bf
rajeshroshanprasad <rajesh.roshan.etc@gmail.com>
2020-06-25 15:32:43 -0700

    working

---
+++ list-divisible.py
@@ -0,0 +1,10 @@
+nums = [22,35,42,41,33, 30]
+
+for i in nums:
+    if i % 5 == 0:
+        print(i)
+        break
+
+else:
+    print("not found")
+
---
+++ prime.py
@@ -0,0 +1,10 @@
+num = int(input("enter the number: "))
+
+for i in range(2, num):
+    if num % i == 0:
+        print("Not prime")
+        break
+else:
+    print("prime")
+
+
